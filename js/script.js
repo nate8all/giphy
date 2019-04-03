7775116334d899e8c771f7f2abfda3ea43238c99
Nathan Alleyne <nathan.alleyne7@gmail.com>
2019-04-03 21:58:51 +0000

    Finished

--- js/script.js
+++ js/script.js
@@ -4,12 +4,15 @@
 /* global $ */
 
 $("#search-button").click(function(){
-  
+var search = $("#search-term").val();
+var randomNum = Math.floor(Math.random() * 10);
 $.ajax({
-    url: "https://api.giphy.com/v1/gifs/search?q=puppy&rating=pg&api_key=dc6zaTOxFJmzC",
+    url: "https://api.giphy.com/v1/gifs/search?q=" + search + "&rating=pg&api_key=dc6zaTOxFJmzC",
     method: "GET",
     success: function(response) {
-        $("body").append(response.data[0].images.original.url);
+        $(".container1").html("<img src=" + response.data[randomNum].images.original.url + ">");
+        $(".container1").append("<br>"+"<a target=_blank href=" + response.data[randomNum].images.original.url + ">Here's the link!</a>");
+        
     }
 }
     
